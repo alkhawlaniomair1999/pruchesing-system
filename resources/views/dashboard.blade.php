61e60fb05c9e193153a0795a965232b3a7c55c92
yaeed77 <aldalalyyazeed@gmai.com>
2025-02-26 06:10:52 +0300

    yazeed

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,14 +1,1 @@
 @extends('include.app')
-@section('title')
-الرئيسية
-@endsection
-
-
-
-@section('page')
-الرئيسية
-@endsection
-
-@section('main')
-<h3>welcome to our project </h3>
-@endsection
--- resources/views/include/saidebar.blade.php
+++ resources/views/include/saidebar.blade.php
@@ -1,8 +1,11 @@
 <div class="sidebar" id="sidebar">
     <h3>القائمة الجانبية</h3>
     <ul>
+        <li> مرحبا:   {{ Auth::user()->name }}<br> </li>
+        <li><hr></li>
         <li><a href="{{url('init')}}" class="fa-solid fa-gear">التهيئة  </a></li>
         <li><a href="#" class="fa-solid fa-circle-user">المستخدمين </a></li>
-        <li><a href="#">رابط 3</a></li>
+        <li><a href="{{url('logout')}}"class="fa-solid fa-right-from-bracket">خروج </a></li>
+    
     </ul>
 </div>
