607b1938f732af4c75d99a0f732fc22862661c00
omair <alkhawlaniomair@gmail.com>
2025-03-01 02:48:06 +0300

    omair

--- app/Http/Controllers/ReportsController.php
+++ app/Http/Controllers/ReportsController.php
@@ -3,13 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\accounts;
+use App\Models\Branch;
+use App\Models\details;
+use App\Models\items;
 
 class ReportsController extends Controller
 {
     public function index()
     {
+        $accounts = accounts::all();
+        $branches = Branch::all();
+        $details = details::all();
+        $items = items::all();
        
-        return view('reports');
+        return view('reports',['accounts'=>$accounts,'branches'=>$branches,'details'=>$details,'items'=>$items]);
     }
 
 
--- resources/views/reports.blade.php
+++ resources/views/reports.blade.php
@@ -1,32 +1,94 @@
 @extends('include.app')
 @section('title')
-    التقارير 
+    التقارير
 @endsection
 
 @section('page')
-    تقارير 
+    تقارير
 @endsection
 @section('main')
 
 
-<h2>جدول عرض البيانات</h2>
+    <h2>جدول عرض البيانات</h2>
     <table>
         <thead>
             <tr>
                 <th>الصنف</th>
                 <th>الاجمالي</th>
-                <th>ابو عريش</th>
-                <th>الاحد</th>
-                <th>صبيا</th>
-                
+                @if (isset($branches))
+                    @foreach ($branches as $b)
+                        <th>{{ $b->branch }}</th>
+                    @endforeach
+                @endif
+
             </tr>
         </thead>
         <tbody>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
+            @if (isset($items))
+                @foreach ($items as $i)
+                    @php
+                        $total = 0;
+                    @endphp
+                    <tr>
+                        <td>{{ $i->item }}</td>
+                        @foreach ($details as $d)
+                            @if ($d->item_id == $i->id)
+                                @php
+                                    $total += $d->total;
+                                @endphp
+                            @endif
+                        @endforeach
+                        <td>{{ $total }}</td>
+                        @if (isset($branches))
+                            @foreach ($branches as $b)
+                                @php
+                                    $total = 0;
+                                @endphp
+                                @foreach ($details as $d)
+                                    @if ($d->item_id == $i->id && $d->branch_id == $b->id)
+                                        @php
+                                            $total += $d->total;
+                                        @endphp
+                                    @endif
+                                @endforeach
+                                <td>{{ $total }}</td>
+                            @endforeach
+                        @endif
+                    </tr>
+                @endforeach
+            @endif
+            <tr>
+                <td>الاجمالي: </td>
+                @php
+                    $total = 0;
+                @endphp
+                @if (isset($details))
+                    @foreach ($details as $det)
+                        @php
+                            $total += $det->total;
+                        @endphp
+                    @endforeach
+                @endif
+                <td>{{ $total }}</td>
+                @if (isset($branches))
+                    @foreach ($branches as $b)
+                        @php
+                            $total = 0;
+                        @endphp
+                        @if (isset($details))
+                            @foreach ($details as $det)
+                                @if ($det->branch_id == $b->id)
+                                    @php
+                                        $total += $det->total;
+                                    @endphp
+                                @endif
+                            @endforeach
+                        @endif
+                        <td>{{ $total }}</td>
+                    @endforeach
+                @endif
+            </tr>
+
         </tbody>
     </table>
 
