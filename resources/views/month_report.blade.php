7665d12c8db667070d77c43b477ead05d37f3e1e
yaeed77 <aldalalyyazeed@gmai.com>
2025-03-01 03:54:09 +0300

    yazeed

--- resources/views/month_report.blade.php
+++ resources/views/month_report.blade.php
@@ -7,6 +7,18 @@
     تقارير
 @endsection
 @section('main')
+<style>
+    select {
+            padding: 10px;
+            font-size: 16px;
+            border-radius: 4px;
+            border: 1px solid #ccc;
+            background-color: #fff;
+            appearance: none; /* إخفاء السهم */
+            background-position: right 10px center;
+            background-repeat: no-repeat;
+        }
+    </style>
     <form action="{{ route('reports.monthly') }}" method="post">
         @csrf
         <label for="month">اختر الشهر:</label>
@@ -18,11 +30,12 @@
 
         <label for="year">اختر السنة:</label>
         <select name="year" id="year">
-            @for ($y = 2020; $y <= now()->year; $y++)
+            @for ($y = 2023; $y <= now()->year; $y++)
                 <option value="{{ $y }}">{{ $y }}</option>
             @endfor
         </select>
+        <button type="submit" class="edit-btn"> عرض التقرير<i class=""></i></button>
 
-        <button type="submit">عرض التقرير</button>
+        
     </form>
 @endsection
