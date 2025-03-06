7315867b6a5ecae89e8ef35ee599526b91ddec23
yaeed77 <aldalalyyazeed@gmai.com>
2025-03-06 08:41:35 +0300

    yazeed

--- resources/views/casher_report.blade.php
+++ resources/views/casher_report.blade.php
@@ -7,26 +7,38 @@
     تقارير
 @endsection
 @section('main')
+<button class="print_btn" onclick="window.print()">طباعة<i class="fa-solid fa-print"></i></button>
 
 
     <h2>تقرير الشهر: {{ $month }}/{{ $year }} للموظف: {{ $c->casher }}</h2>
     <table>
         <thead>
             <tr>
+                <th>التاريخ</th>
                 <th>المبيعات</th>
                 <th>البنك</th>
                 <th>الكاش</th>
                 <th>المصروفات</th>
-                <th>عجز او زيادة</th>
-                <th>التاريخ</th>
+                <th>عجز-زيادة</th>
             </tr>
 
         </thead>
         <tbody>
+@php
+    $sum_total=0;
+    $sum_bank=0;
+    $sum_cash=0;
+    $sum_out=0;
+    $sum_plus=0;
+@endphp
+
+
             @if (isset($casher_proc))
                 @foreach ($casher_proc as $cp)
                    
                     <tr>
+                        <td>{{ $cp->date }}</td>
+
                         <td>{{ $cp->total }}</td>
                 
                         <td>{{ $cp->bank }}</td>
@@ -34,11 +46,36 @@
                         <td>{{ $cp->out }}</td>
                         <td>{{ $cp->plus }}</td>
 
-                        <td>{{ $cp->date }}</td>
 
             </tr>
+@php
+    $sum_total+=$cp->total;
+    $sum_bank+=$cp->bank;
+    $sum_cash+=$cp->cash;
+    $sum_out+=$cp->out;
+    $sum_plus+=$cp->plus;
+@endphp
+
+
 @endforeach
 @endif
+
+<tfoot>
+<tr>
+    <th>الاجمالي:</th>
+<th>{{$sum_total}}</th>
+<th>{{$sum_bank}}</th>
+<th>{{$sum_cash}}</th>
+<th>{{$sum_out}}</th>
+<th>{{$sum_plus}}</th>
+
+
+</tr>
+
+
+</tfoot>
+
+
         </tbody>
     </table>
 
