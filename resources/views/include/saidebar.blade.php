9db19ded51125c9aefceb2d8c0cd6d24311bdcce
yazeed <aldalalyyazeed@gmail.com>
2025-03-19 02:49:11 +0300

    yazeed

--- resources/views/include/saidebar.blade.php
+++ resources/views/include/saidebar.blade.php
@@ -6,10 +6,10 @@
         <li></li>
         <li><a href="{{ url('init') }}"> <i class="fa-solid fa-gear"></i>التهيئة </a></li>
         <li><a href="{{ url('supplier') }}"><i class="fa-solid fa-truck"></i>التوريد </a></li>
-        <li><a href="{{ url('pay') }}"><i class="fa-solid fa-truck"></i>قيد تسديد </a></li>
+        <li><a href="{{ url('pay') }}"><i class="fa-solid fa-hand-holding-dollar"></i> قيد تسديد </a></li>
         <li><a href="{{ url('details') }}"><i class="fa-solid fa-calendar-check"></i>التسجيل اليومي </a></li>
         <li><a href="{{ url('casher_proc') }}"><i class="fa-solid fa-cash-register"></i>عمليات الكاشير </a></li>
-        <li><a href="{{ url('reports') }}"><i class="fa fa-info-circle"></i>التقارير </a></li>
+        <li><a href="{{ url('reports') }}"><i class="fa fa-chart-bar"></i>التقارير </a></li>
         <li><a href="{{ url('users') }}"><i class="fa-solid fa-circle-user"></i>المستخدمين </a></li>
     </ul>
 </div>
